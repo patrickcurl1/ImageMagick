7f8ecb755e6c006064bf4f2ffca3c408eaaccb68
patrickcurl@hotmail.com <patrickcurl@hotmail.com>
2020-12-21 15:26:34 -0600

    >adding in the 3 scripts jeff would like for testing

---
+++ scripts/1-start-smarttrim.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+cd /Images
+find . -name "*.tif" -print0 | while read -d $'\0' tiffile
+do
+mkdir /Images/out
+echo "$tiffile"
+tifpath=${tiffile:2}
+echo $tifpath
+tifout=./out/$tifpath
+echo $tifout
+filepath="$(dirname "${tifout}")"
+echo $filepath
+if [ ! -d $filepath ]
+then
+  mkdir -p $filepath
+fi
+
+if [ -f "$tifout" ]; then
+echo "$tifout exists."
+else 
+echo "$tifout does not exists."
+bash /scripts/smarttrim $tiffile $tifout
+fi
+done
