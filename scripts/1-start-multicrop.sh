2a4d7529ac191f93ab6cf638da54728c20215888
patrickcurl@hotmail.com <patrickcurl@hotmail.com>
2020-12-23 09:17:15 -0600

    fixing infinite loop properly, adding verbiage to the output and variables, logging becomes more redable

--- scripts/1-start-multicrop.sh
+++ scripts/1-start-multicrop.sh
@@ -1,24 +1,29 @@
 #! /usr/bin/env bash
 cd /Images
+counter=1
+until [ $counter -lt 0 ]
+do
 find . -name "*.tif" -not -path "./out/*" -print0 | while read -d $'\0' tiffile
 do
 mkdir /Images/out
-echo "$tiffile"
+echo "image to be processed = $tiffile"
 tifpath=${tiffile:2}
-echo $tifpath
+echo "image path = $tifpath"
 tifout=./out/$tifpath
-echo $tifout
+echo "image out path = $tifout"
 filepath="$(dirname "${tifout}")"
-echo $filepath
+echo "image file path = $filepath"
 if [ ! -d $filepath ]
 then
   mkdir -p $filepath
 fi
 
 if [ -f "$tifout" ]; then
-echo "$tifout exists."
+echo "$tifout exists, moving on to the next image."
 else 
-echo "$tifout does not exists."
+echo "$tifout does not exists creating image."
 bash /scripts/multicrop $tiffile $tifout
 fi
+done
+((counter++))
 done
